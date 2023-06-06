52edfd68d2ac95b6e9a0ccbcc91650fe7395a65f
Anmol <anmol36pandey@gmail.com>
2023-06-06 23:36:06 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":'5e1098bcd4mshbd1bd6553839c3ap192559jsn14d8ac6fb9fe',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
