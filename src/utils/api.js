7bff7af03ceeaafa870238f06a8f280d99f3d96f
Anmol <anmol36pandey@gmail.com>
2024-11-05 21:30:08 +0530

    api change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL =process.env.REACT_APP_BASE_URL;
 const options = {
-    params: { hl: "en", gl: "US" },
+    //params: { hl: "en", gl: "US" },
+    method: 'GET',
     headers: {
         "X-RapidAPI-Key":process.env.REACT_APP_API_KEY,
         "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
@@ -10,6 +11,7 @@ const options = {
 };
 
 export const fetchDataFromApi = async (url) => {
+    console.log("anmol ",BASE_URL);
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     return data;
 };
